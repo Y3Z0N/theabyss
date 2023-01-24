0852c1230ca8e3dbe3e28f8ded8dbb6d22612062
KhanhPham05 <68932626+KhanhPham05@users.noreply.github.com>
2023-01-24 18:56:57 +0700

    Fixing some conflicts

--- src/main/java/net/yezon/theabyss/events/AppleOfImmortalityEffectEvent.java
+++ src/main/java/net/yezon/theabyss/events/AppleOfImmortalityEffectEvent.java
@@ -6,10 +6,8 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
 
-public class AppleOfImmortalityEffectevent {
+public class AppleOfImmortalityEffectEvent {
 	public static void execute(Entity entity) {
-		if (entity == null)
-			return;
 		if (entity instanceof LivingEntity _entity)
 			_entity.addEffect(new MobEffectInstance(TheabyssModMobEffects.IMMORTAL.get(), 110, 1));
 	}
